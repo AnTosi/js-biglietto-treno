a8d6c763b8127ecab07ec3861f6996b5e4808321
AnTosi <andrea.tosi.uni@gmail.com>
2021-10-11 15:27:37 +0200

    esercizio fatto, ora provo a migliorare cosine

---
+++ main.js
@@ -0,0 +1,24 @@
+const km = prompt("Quanti chilometri devi percorrere?");
+console.log(km);
+
+const age = prompt("Quanti anni hai?");
+console.log(age);
+
+var basePrice = (km * 0.21);
+console.log(basePrice);
+
+if (age < 18) {
+    var finalPrice = (basePrice * 0.8);
+}
+
+if (age > 65) {
+    var finalPrice = (basePrice * 0.6);
+}
+
+if (age >= 18 && age <= 65) {
+    var finalPrice = basePrice;
+}
+
+var finalPrice = parseFloat(finalPrice).toFixed(2);
+
+console.log(finalPrice)
